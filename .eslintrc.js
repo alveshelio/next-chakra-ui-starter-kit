418b5db0cca839879738c08014d3b6dc6d2915dc
Helio <helios25@gmail.com>
2021-02-03 19:00:47 +0000

    :rotating_light: Turned off react/no-unescaped-entities

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,16 +30,13 @@ module.exports = {
       rules: {
         // We will use TypeScript's types for component props instead
         'react/prop-types': 'off',
-
         // No need to import React when using Next.js
+        'react/no-unescaped-entities': 'off',
         'react/react-in-jsx-scope': 'off',
-
         // This rule is not compatible with Next.js's <Link /> components
         'jsx-a11y/anchor-is-valid': 'off',
-
         // Why would you want unused vars?
         '@typescript-eslint/no-unused-vars': ['error'],
-
         // I suggest this setting for requiring return types on functions only where useful
         '@typescript-eslint/explicit-function-return-type': [
           'warn',
