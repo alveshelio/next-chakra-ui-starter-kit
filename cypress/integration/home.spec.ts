bf2b50b0fd78467ac4ac20b956fb1d0740deee5c
Helio <helios25@gmail.com>
2021-02-03 20:38:00 +0000

    :white_check_mark: added test to toggle theme in home page

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -6,4 +6,9 @@ describe('Home Page', () => {
   it('checks the page loads correctly and Dark is present', () => {
     cy.dataCy('dark').should('contain.text', 'Dark')
   })
+
+  it('toggles the theme from dark to light', () => {
+    cy.dataCy('theme-toggle').click()
+    cy.get('body').should('have.class', 'chakra-ui-light')
+  })
 })
