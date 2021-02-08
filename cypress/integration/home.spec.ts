b243c22fb010aab4acceeeb02b33dd4077747c4e
Helio <helios25@gmail.com>
2021-02-08 21:34:07 +0000

    :construction_worker: updated url in cy.visit()

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,6 +1,6 @@
 describe('Home Page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
   })
 
   it('Checks the page loads correctly and Dark is present', () => {
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '@modules/(.*)': 'src/modules/$1',
+    '@theme/(.*)': 'src/theme/$1',
+  },
 }
